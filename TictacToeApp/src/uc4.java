b7624ce7ef58d8fcf78157fcd65338010dbfbb09
kameshsaravanan852005-glitch <kameshsaravanan852005@gmail.com>
2026-04-21 11:24:31 +0530

    uc4 added

---
+++ TictacToeApp/src/uc4.java
@@ -0,0 +1,21 @@
+public class uc4
+{
+
+    // Utility method to convert slot to row & column
+    public static int[] getRowCol(int slot) {
+        int row = (slot - 1) / 3;
+        int col = (slot - 1) % 3;
+        return new int[]{row, col};
+    }
+
+    public static void main(String[] args) {
+
+        int slot = 5; // Example input
+
+        int[] position = getRowCol(slot);
+
+        System.out.println("Slot: " + slot);
+        System.out.println("Row: " + position[0]);
+        System.out.println("Column: " + position[1]);
+    }
+}
