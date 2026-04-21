c5f4c3f138aacbc2242072e3fcda0e4c3d916bfe
kameshsaravanan852005-glitch <kameshsaravanan852005@gmail.com>
2026-04-21 14:45:38 +0530

    uc5 added

---
+++ TictacToeApp/src/uc5.java
@@ -0,0 +1,18 @@
+public class uc5 {
+
+    // Method to validate move
+    public static boolean isValidMove(char[][] board, int row, int col) {
+
+        // Boundary check
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+
+        // Check if cell is empty
+        if (board[row][col] != '-') {
+            return false;
+        }
+
+        return true;
+    }
+}
